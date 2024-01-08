4f130de17dd869cbec1ecbaa58a640015877dfa9
Szymon Chyc <erressrsch@gmail.com>
2024-01-08 23:26:57 +0100

    Add project files.

---
+++ mod2_les8_task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main ()
+    {
+        int number = 0;
+
+        do
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(number);
+            }
+            number++;
+
+        }
+        while (number <= 1000);
+
+    }
+}
